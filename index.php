ba70d3f0cb94408f8f741fb9f4f4b87042d3b4fb
josh-ahey <josh.ahey@gmail.com>
2016-04-18 19:32:33 +0000

    first page
    Rendered the index page using twig

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+require_once './Twig/Autoloader.php';
+Twig_Autoloader::register();
+
+$loader = new Twig_Loader_Filesystem('./template');
+$twig = new Twig_Environment($loader);
+
+echo $twig->render("index.html",[
+                      
+]);
+?>
